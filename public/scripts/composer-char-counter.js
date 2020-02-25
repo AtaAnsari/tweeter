df355eee1608db46b72e22d49b789822be79085a
AtaAnsari <ataulhaq.ansari@gmail.com>
2020-02-25 21:25:01 +0000

    the counter now changes colour when char count is over 140

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,6 +5,13 @@ $(document).ready(function() {
     const count = $(this).val().length
     const $counter = $(this).parent().find(".counter");
     $counter.text(140 - count)
+    if(count > 140) {
+      $counter.css("color", "red")
+    } else { 
+      $counter.css("color", "#545149")
+    }
   });
+
+
 
 });
